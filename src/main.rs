a4f5a7f82cc6518a599bbd02b1d22695d72d4b11
André Wronscki <andrewronscki@gmail.com>
2024-07-26 19:55:45 -0300

    adicionando test e adicionando pipe de cobertura de teste

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+fn soma(a: i32, b: i32) -> i32 {
+    a + b
+}
+
 fn main() {
-    println!("Hello, André!");
+    let soma = soma(10, 5);
+    println!("A soma é: {}", soma);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_soma() {
+        assert_eq!(soma(2, 2), 4);
+        assert_eq!(soma(-2, 2), 0);
+        assert_eq!(soma(0, 0), 0);
+        assert_eq!(soma(10, 5), 15);
+    }
 }
